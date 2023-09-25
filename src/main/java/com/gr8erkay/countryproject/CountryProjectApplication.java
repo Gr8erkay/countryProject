62989b9eb30b98006dce35f58870b99baaa66378
eomokayode <eomokayode@sysserve.com>
2023-09-25 01:02:46 +0100

    Initial commit

---
+++ src/main/java/com/gr8erkay/countryproject/CountryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gr8erkay.countryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gr8erkay/countryproject/CountryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr8erkay.countryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
